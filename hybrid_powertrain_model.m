b1023f18395e0253c8a045254be8150fc9fbd764
Muhammad Wasib <mhmdwasib@gmail.com>
2026-09-24 01:48:19 +0100

    Add MATLAB script for language detection

---
+++ hybrid_powertrain_model.m
@@ -0,0 +1,5 @@
+%% Series Hybrid Electric Vehicle Powertrain Model
+% MATLAB/Simulink based SHEV simulation
+% Energy management controller and battery SOC estimation
+
+disp("Series Hybrid Powertrain Simulation");
